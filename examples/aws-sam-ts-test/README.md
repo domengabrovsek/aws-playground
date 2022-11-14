74c521812b06f941c8c7a0fb8ad42d8366507a09
Domen Gabrovsek <domen.gabrovsek@1337.tech>
2022-11-14 17:45:36 +0100

    aws-sam-ts-test

---
+++ examples/aws-sam-ts-test/README.md
@@ -0,0 +1,1 @@
+# aws-sam-ts-test
---
+++ examples/aws-sam-ts-test/src/app.ts
@@ -0,0 +1,13 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult
+} from "aws-lambda";
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const queries = JSON.stringify(event.queryStringParameters);
+  return {
+    statusCode: 200,
+    body: `Queries: ${queries}`
+  }
+}
